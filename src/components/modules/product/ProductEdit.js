efc19013c94c9da29c2e42e4fbcfb201f6027034
Al Ekram Hossain Abir <alabir65@gmail.com>
2024-06-14 12:22:32 +0600

    fixed select field

--- src/components/modules/product/ProductEdit.js
+++ src/components/modules/product/ProductEdit.js
@@ -3,7 +3,7 @@ import Breadcrumb from "../../partoals/Breadcrumb";
 import Constants from "../../../Constants";
 import Swal from "sweetalert2";
 import CardHeader from "../../partoals/miniComponents/CardHeader";
-import { Link, redirect, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Select from "react-select";
 import ReactQuill from "react-quill";
@@ -20,7 +20,6 @@ const ProductEdit = () => {
     const [subCategories, setSubCategories] = useState([]);
     const [childSubCategories, setChildSubCategories] = useState([]);
     const [brands, setBrands] = useState([]);
-    const [addProductData, setAddProductData] = useState([]);
     const [allSubcategories, setAllSubcategories] = useState([]);
     const [allChildSubcategories, setAllChildSubcategories] = useState([]);
     const [countries, setCountries] = useState([]);
@@ -35,10 +34,8 @@ const ProductEdit = () => {
     const [selectedShops, setSelectedShops] = useState([]);
     const [quantities, setQuantities] = useState({});
     const [totalStock, setTotalStock] = useState(0);
-    const [product, setProduct] = useState([]);
     const [input, setInput] = useState({});
     const [attributeShopQuantities, setAttributeShopQuantities] = useState({});
-    const [multiValue, setMultiValue] = useState();
 
     const handleDescriptionChange = (value) => {
         setInput((prevState) => ({
@@ -64,6 +61,7 @@ const ProductEdit = () => {
                 const costValue = response.data.data.cost.replace(/[৳,]/g, "");
                 const priceValue = response.data.data.price.replace(/[৳,]/g, "");
                 const shopData = response.data.data.shops;
+                console.log("Edit Data",response.data.data)
 
                 // Filter out duplicate shops based on shop_id
                 const uniqueShopData = [];
@@ -464,6 +462,25 @@ const ProductEdit = () => {
         console.log(row_no);
         console.log('***');
         console.log(e);
+        // setAttributeField((prev) => {
+        //     return prev.map(item => {
+        //       // manipulate shop_quantities here
+        //       let newShopQuantities = item.shop_quantities.map(shopQuantity => {
+        //         // return a new shopQuantity object
+        //         return {
+        //           ...shopQuantity,
+        //           // update properties here
+        //         };
+        //       });
+        //       console.log(newShopQuantities)
+          
+        //       // return a new item object with the updated shop_quantities
+        //       return {
+        //         ...item,
+        //         shop_quantities: newShopQuantities,
+        //       };
+        //     });
+        //   });
         setAttributeShopQuantities({
             ...attributeShopQuantities,
             [row_no]: e
@@ -880,7 +897,7 @@ const ProductEdit = () => {
                                                                     </label>
                                                                 </div>
                                                                 <div className="col-md-2">
-                                                                    <Select
+                                                                    {/* <Select
                                                                         options={shops} // Ensure 'shops' is in the format [{ value: 1, label: "Main Branch" }, ...]
                                                                         isMulti
                                                                         // onChange={(e) => { handleAttributeShopChange(id, e) }}
@@ -895,13 +912,26 @@ const ProductEdit = () => {
                                                                         // onChange={(e) => { handleAttributeShopChange(value?.id, e) }}
                                                                         className="mb-3"
                                                                         placeholder="Select Shops"
-                                                                    />
+                                                                        value={selectedShops}
+                                                                    /> */}
 
-                                                                    {/* {
-                                                                        value?.shop_quantities.map((shop, index) => {
+                                                                    {
+                                                                        value?.shop_quantities.map((shop) => {
                                                                             const inputName = `shop_quantity_${shop.shop_id}`;
+                                                                            const shopName = {
+                                                                                value: shop.shop_id,
+                                                                                label: shop.shop_name
+                                                                            }
                                                                             return (
                                                                                 <div key={shop.shop_id} className="mb-2">
+                                                                                    <Select
+                                                                                        options={shops} // Ensure 'shops' is in the format [{ value: 1, label: "Main Branch" }, ...]
+                                                                                        isMulti
+                                                                                        onChange={(e) => { handleAttributeShopChange(index, e) }}
+                                                                                        className="mb-3"
+                                                                                        placeholder="Select Shops"
+                                                                                        value={shopName}
+                                                                                    />
                                                                                     <label>{shop.shop_name} Quantity</label>
                                                                                     <input
                                                                                         type="number"
@@ -913,7 +943,7 @@ const ProductEdit = () => {
                                                                                 </div>
                                                                             );
                                                                         })
-                                                                    } */}
+                                                                    }
                                                                 </div>
 
                                                                 <div className="col-md-2">
